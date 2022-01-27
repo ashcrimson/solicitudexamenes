5ba111fdce34011f30e02a98c7e538a231a3c8fe
Felipe Pino <felipepinoredes@gmail.com>
2022-01-27 17:21:46 -0300

    cambios minimos

--- resources/views/examen_tipos/index.blade.php
+++ resources/views/examen_tipos/index.blade.php
@@ -9,7 +9,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Examen Tipos</h1>
+                    <h1>Examenes</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-right">
