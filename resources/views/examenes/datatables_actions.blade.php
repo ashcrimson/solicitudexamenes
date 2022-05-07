ebb985a2e35e0b6ac7cfe34d99d14e187e36371a
Josue Martinez <m.josue3443@gmail.com>
2022-05-06 20:23:27 -0600

    se valida cuando mostrar botones de datatable

--- resources/views/examenes/datatables_actions.blade.php
+++ resources/views/examenes/datatables_actions.blade.php
@@ -1,20 +1,25 @@
 @can('Ver Examens')
-<a href="{{ route('examenes.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-default btn-sm'>
-    <i class="fa fa-eye"></i>
-</a>
+    <a href="{{ route('examenes.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-default btn-sm'>
+        <i class="fa fa-eye"></i>
+    </a>
 @endcan
 
 @can('Editar Examens')
-<a href="{{ route('examenes.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-outline-info btn-sm'>
-    <i class="fa fa-edit"></i>
-</a>
+    @if($examen->estado_id == \App\Models\ExamenEstado::INGRESADO ||
+        $examen->estado_id == \App\Models\ExamenEstado::SOLICITADO)
+        <a href="{{ route('examenes.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-outline-info btn-sm'>
+            <i class="fa fa-edit"></i>
+        </a>
+    @endif
 @endcan
 
 @can('Eliminar Examens')
-<a href="#" onclick="deleteItemDt(this)" data-id="{{$id}}" data-toggle="tooltip" title="Eliminar" class='btn btn-outline-danger btn-sm'>
-    <i class="fa fa-trash-alt"></i>
-</a>
-
+    @if($examen->estado_id == \App\Models\ExamenEstado::INGRESADO ||
+        $examen->estado_id == \App\Models\ExamenEstado::SOLICITADO)
+            <a href="#" onclick="deleteItemDt(this)" data-id="{{$id}}" data-toggle="tooltip" title="Eliminar" class='btn btn-outline-danger btn-sm'>
+                <i class="fa fa-trash-alt"></i>
+            </a>
+    @endif
 
 <form action="{{ route('examenes.destroy', $id)}}" method="POST" id="delete-form{{$id}}">
     @method('DELETE')
